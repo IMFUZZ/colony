0d4c295040d0d2776a5dbf4336ff7ab516f8c373
guillaumeboudreau <gbchromebook@gmail.com>
2016-12-14 10:42:54 -0500

    Ajout des test pour node et link

--- src/game/UnitTest/NodeTest.ts
+++ src/game/UnitTest/NodeTest.ts
@@ -3,4 +3,31 @@ class NodeTest extends UnitTest {
     {
         super(selector, NodeTest.name);
     }
+
+    testAddLink()
+    {
+        let node = new NodeEntity(10,10);
+        super.assertTrue(node.links.length == 0);
+        let link = new Link(null,null);
+        node.addLink(link);
+        super.assertTrue(node.links.length == 1);
+        super.assertTrue(node.links[0] == link);
+    }
+
+    testBelongsTo()
+    {
+        let color = 0xffffff
+        let player = new Player(color,22);
+        let node = new NodeEntity(10,10);
+        super.assertTrue(!node.belongsTo(player));
+        node.setOwner(player);
+        super.assertTrue(node.belongsTo(player));
+        super.assertTrue(node.color == color);
+    }
 }
+
+$(document).ready(function()
+{
+    let test = new NodeTest("#results");
+    test.run();
+})
